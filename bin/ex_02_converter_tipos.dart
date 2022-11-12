fddb1b8a7afdf9d58e0e7972ff064a6058dc4d40
caiogmrocha <caioguilherme739@gmail.com>
2022-11-12 18:05:35 -0300

    exercício 002: converção entre tipos de dados

---
+++ bin/ex_02_converter_tipos.dart
@@ -0,0 +1,17 @@
+void main() {
+  // Inteiro para String
+  int n1 = 123;
+  print(n1.toString());
+
+  // String para Inteiro
+  String n2 = '456';
+  print(int.parse(n2));
+
+  // Decimal para String
+  double n3 = 7.89;
+  print(n3.toString());
+
+  // String para Decimal
+  String n4 = '3.1';
+  print(double.parse(n4));
+}
